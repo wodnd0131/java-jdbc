768bdcb416a9787a7158f96791654157730b42f5
wddy2001 <wodnd0131@naver.com>
2025-10-26 19:59:51 +0900

    feat: Service 단에서 트랜잭션을 관리하도록 구현

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,11 +1,18 @@
 package com.techcourse.service;
 
+import com.interface21.dao.DataAccessException;
+import com.interface21.jdbc.datasource.ConnectionManager;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
+import java.sql.SQLException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UserService {
+
+    private static final Logger log = LoggerFactory.getLogger(UserService.class);
 
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
@@ -24,9 +31,38 @@ public class UserService {
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+        runInTransaction(() -> {
+            final var user = findById(id);
+            user.changePassword(newPassword);
+            userDao.update(user);
+            userHistoryDao.log(new UserHistory(user, createBy));
+        });
+    }
+
+    public void runInTransaction(Runnable action) {
+        final var connectionManager = ConnectionManager.getInstance();
+        final var connection = connectionManager.getConnection();
+        if (connection.isPresent()) {
+            final var conn = connection.get();
+            try {
+                conn.setAutoCommit(false);
+                action.run();
+                conn.commit();
+            } catch (SQLException e) {
+                try {
+                    conn.rollback();
+                } catch (SQLException ex) {
+                    log.error("Rollback failed: {}", e.getMessage());
+                }
+                throw new DataAccessException("Transaction failed: " + e.getMessage());
+            } finally {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    log.error("Connection Close failed: {}", e.getMessage());
+                }
+                connectionManager.setConnection(null);
+            }
+        }
     }
 }
